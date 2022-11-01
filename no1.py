75903798fd23b255f1ddb3202b5c3752c92b8c4d
ajengprastia <115528189+ajengprastia@users.noreply.github.com>
2022-11-01 14:05:27 +0700

    Add files via upload

---
+++ no1.py
@@ -0,0 +1,12 @@
+#input
+nama = input("Nama: ")
+email = input("Email: ")
+jabatan = input("Jabatan: ")
+umur = input("Umur: ")
+
+#output
+print ("   ");
+print ("Nama: "+nama);
+print ("Email: "+email);
+print ("Jabatan: "+jabatan);
+print ("Umur: "+umur);
